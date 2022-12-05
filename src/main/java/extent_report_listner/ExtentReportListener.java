8d401d324ff7470a0bb5ee5af545e8481e731fd4
Walaa Jumaa <w.jumaa@Amcan.local>
2022-12-05 10:38:50 +0300

    adding some codes related to the report

--- src/main/java/extent_report_listner/ExtentReportListener.java
+++ src/main/java/extent_report_listner/ExtentReportListener.java
@@ -36,10 +36,9 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 public class ExtentReportListener implements IReporter {
 
 
-        private ExtentReports extent;
-       private ExtentSparkReporter spark;
-       private ExtentTest test;
-
+    private ExtentReports extent;
+    private ExtentSparkReporter spark;
+    private ExtentTest test;
 
 
     String pattern = "yyyy-MM-dd";
@@ -48,88 +47,88 @@ public class ExtentReportListener implements IReporter {
 
 
     public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
-                                   String outputDirectory ) {
+                               String outputDirectory) {
 
         //The date will include with the file name
-            extent = new ExtentReports("C:\\Users\\w.jumaa\\Desktop\\Automation Reports" + File.separator
-                    + "Magento-Sporter-Automation_Report "+"["+BaseTest.environmentName+"]"+date+".html", true);
+        extent = new ExtentReports("C:\\Users\\w.jumaa\\Desktop\\Automation Reports" + File.separator
+                + "Magento-Sporter-Automation_Report " + "[" + BaseTest.environmentName + "]" + date + ".html", true);
 
 //            spark=new ExtentSparkReporter(File.separator + "Magento-Sporter-Automation_Report "+date+".html");
 //            spark.viewConfigurer().viewOrder().as(new ViewName[]{ViewName.DASHBOARD, ViewName.TEST,ViewName.CATEGORY}).apply();
 
-            extent.addSystemInfo("Environment", BaseTest.siteURL);
-            extent.addSystemInfo("Author","Wala'a Mohammad");
-
-
-
+        extent.addSystemInfo("Environment", BaseTest.siteURL);
+        extent.addSystemInfo("Author", "Wala'a Mohammad");
 
 
         //loading the external xml file
-        extent.loadConfig(new File(System.getProperty("user.dir")+"./src/test/resources/extent-config.xml"));
+        extent.loadConfig(new File(System.getProperty("user.dir") + "./src/test/resources/extent-config.xml"));
 
 
-            for (ISuite suite : suites) {
-                Map<String, ISuiteResult> result = suite.getResults();
+        for (ISuite suite : suites) {
+            Map<String, ISuiteResult> result = suite.getResults();
 
-                for (ISuiteResult r : result.values()) {
-                    ITestContext context = r.getTestContext();
+            for (ISuiteResult r : result.values()) {
+                ITestContext context = r.getTestContext();
 
-                    buildTestNodes(context.getPassedTests(), LogStatus.PASS);
-                    buildTestNodes(context.getFailedTests(), LogStatus.FAIL);
-                    buildTestNodes(context.getSkippedTests(), LogStatus.SKIP);
+                buildTestNodes(context.getPassedTests(), LogStatus.PASS);
+                buildTestNodes(context.getFailedTests(), LogStatus.FAIL);
+                buildTestNodes(context.getSkippedTests(), LogStatus.SKIP);
 
-                }
             }
-
-            extent.flush();
-            extent.close();
         }
 
-        private void buildTestNodes(IResultMap tests, LogStatus status) {
-            ExtentTest test;
+        extent.flush();
+        extent.close();
+    }
 
-            if (tests.size() > 0) {
-                for (ITestResult result : tests.getAllResults()) {
-                    test = extent.startTest(result.getMethod().getDescription());
+
+    private void buildTestNodes(IResultMap tests, LogStatus status) {
+        ExtentTest test;
+
+        if (tests.size() > 0) {
+            for (ITestResult result : tests.getAllResults()) {
+                test = extent.startTest(result.getMethod().getDescription());
 //                    test = extent.startTest(result.getMethod().getMethodName());
 
-                    test.setStartedTime(getTime(result.getStartMillis()));
-                    test.setEndedTime(getTime(result.getEndMillis()));
+                test.setStartedTime(getTime(result.getStartMillis()));
+                test.setEndedTime(getTime(result.getEndMillis()));
 
-                    for (String group : result.getMethod().getGroups())
-                        test.assignCategory(group);
+                for (String group : result.getMethod().getGroups())
+                    test.assignCategory(group);
 
-                    if (result.getThrowable() != null) {
-                        test.log(status, result.getThrowable());
-                    } else {
-                        test.log(status, "Test " + status.toString().toLowerCase()
-                                + "ed");
-                    }
-                    extent.endTest(test);
+                if (result.getThrowable() != null) {
+                    test.log(status, result.getThrowable());
+                } else {
+                    test.log(status, "Test " + status.toString().toLowerCase()
+                            + "ed");
                 }
+                extent.endTest(test);
             }
-
-
         }
 
-        private Date getTime(long millis) {
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTimeInMillis(millis);
-            return calendar.getTime();
-        }
+
+    }
+
+    private Date getTime(long millis) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(millis);
+        return calendar.getTime();
+    }
+
     //This method is to capture the screenshot and return the path of the screenshot.
-    public static String getScreenshot(WebDriver driver, String screenshotName) throws Exception {
+    public static String getScreenshot(WebDriver driver, String screenshotName, ITestResult iTestResult) throws Exception {
         //below line is just to append the date format with the screenshot name to avoid duplicate names
         String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        String failedTest = iTestResult.getName();
+
         TakesScreenshot ts = (TakesScreenshot) driver;
         File source = ts.getScreenshotAs(OutputType.FILE);
         //after execution, you could see a folder "FailedTestsScreenshots" under src folder
-        String destination = System.getProperty("user.dir") + "./FailedTestsScreenshots/"+screenshotName+dateName+".png";
+        String destination = System.getProperty("user.dir") + "./FailedTestsScreenshots/" + screenshotName + dateName + ".png";
         File finalDestination = new File(destination);
         FileUtils.copyFile(source, finalDestination);
         //Returns the captured file path
         return destination;
+
     }
-
-
 }
