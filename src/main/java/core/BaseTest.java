5105674904e37de9e17d089b737f3f12ead42b81
jumahwalaa@gmail.com <W@laa1041994>
2023-02-02 15:12:22 +0300

    Add Broswer Headless code

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -4,11 +4,14 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.ITestContext;
 import org.testng.annotations.*;
 import org.testng.annotations.Parameters;
+
 
 public class BaseTest {
 //    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
@@ -54,7 +57,7 @@ public class BaseTest {
             }
         }
         catch (NoSuchElementException ex){
-//            System.out.println(e.getMessage());
+            System.out.println(ex.getMessage());
 try{
     WebElement btnCloseElement = webDriver.findElement(By.id("btnClose"));
     DataHelperAndWait.waitToBeVisible(btnCloseElement,6,webDriver);
@@ -64,51 +67,10 @@ try{
         btnCloseElement.click();
     }}
      catch (NoSuchElementException e){
-        //            System.out.println(e.getMessage());
+    System.out.println(e.getMessage());
 
     }
-
-
-
-
-
-
     }}
-//    The Below Methods we need to run the TCs across the broswers
-//    @BeforeClass(alwaysRun = true)
-//    @Parameters({"environment","browser"})
-//    public void setupBrowser( String environment ,String browser) throws Exception {
-//    BasePage.siteURL = environment;
-//        System.out.println(browser);
-////Check if parameter passed from TestNG is 'firefox'
-//        switch (browser) {
-////    Check if parameter passed from TestNG is 'firefox'
-//
-//            case "firefox":
-//                System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver.exe");
-//                webDriver = new FirefoxDriver();
-//                webDriver.manage().window().maximize();
-//                webDriver.navigate().to(BasePage.siteURL);
-//                break;
-//            case "chrome":
-//                System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
-//                webDriver = new ChromeDriver();
-//                webDriver.manage().window().maximize();
-//                webDriver.navigate().to(BasePage.siteURL);
-//                break;
-//            case "Edge":
-//                System.setProperty("webdriver.edge.driver", "src/test/resources/drivers/msedgedriver.exe");
-//                webDriver = new EdgeDriver();
-//                webDriver.manage().window().maximize();
-//                webDriver.navigate().to(BasePage.siteURL);
-//                break;
-//            default:
-//////If no browser passed throw exception
-//                throw new Exception("Browser is not correct");
-//        }
-//    }
-
-    // The Below Method to run the TCs on Onc Browser like Chrome
 
     @BeforeClass(alwaysRun = true)
     @Parameters({"environment", "browser", "country","bogoProduct","oOSProduct","bundleProductUrl"})
@@ -127,10 +89,22 @@ try{
             case "firefox":
                 System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver.exe");
                 webDriver = new FirefoxDriver();
+           //FirefoxDriver headless
+//                FirefoxOptions options  = new FirefoxOptions();
+//                options.addArguments("headless");
+//                webDriver = new FirefoxDriver(options);
                 break;
             case "chrome":
                 System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-                webDriver = new ChromeDriver();
+//                webDriver = new ChromeDriver();
+
+                //Chrome headless
+                //ChromeOptions object
+                ChromeOptions opt = new ChromeOptions();
+                //headless parameter
+                opt.addArguments("headless");
+                // set parameter to Chrome driver
+                 webDriver = new ChromeDriver(opt);
                 break;
             case "edge":
                 System.setProperty("webdriver.edge.driver", "src/test/resources/drivers/msedgedriver.exe");
@@ -145,15 +119,6 @@ try{
         webDriver.navigate().to(environment+"/"+country);
         this.CloseInitialDialog();
     }
-
-
-
-
-
-
-
-
-
 //    @BeforeGroups(groups = "All Smoke Testing Result")
 //    @Parameters({"environment"})
 //
