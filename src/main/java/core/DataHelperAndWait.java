d1627e772d5af64e1e582ec272bdbffda2923677
Wala'a Mohammad <jumahwalaa@gmail.com>
2023-03-05 13:57:25 +0300

    Change Workspace file from test device

--- src/main/java/core/DataHelperAndWait.java
+++ src/main/java/core/DataHelperAndWait.java
@@ -16,7 +16,7 @@ import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public  class DataHelperAndWait  {
-    private static int WaitTime=16;
+    private static int WaitTime=20;
     //test4
 
     public static   void waitForElement(WebElement element, WebDriver webDriver) {
