b9df141bc04ddf23d56367b4cdafe21128c3a59d
Wala'a Mohammad <jumahwalaa@gmail.com>
2023-03-06 11:58:54 +0300

    Update Cart test cases

--- src/test/java/sporter_test/cartTestCases/CartTestCases.java
+++ src/test/java/sporter_test/cartTestCases/CartTestCases.java
@@ -326,7 +326,7 @@ public class CartTestCases extends BaseTest {
         DataHelperAndWait.clearWebField(cartPage.getCouponCodeField());
         DataHelperAndWait.clickOnElement(cartPage.getApplyCouponCodeBtn(),webDriver);
         DataHelperAndWait.isDisplayed(cartPage.getRequiredCouponMsg() ,webDriver);
-        cartPage.removeItem();
+//        cartPage.removeItem();
     }
     @Test(groups = {"All Smoke Testing Result","1.3 Medium Severity"},description = "{{CountryName}}: Make sure that the free gift is not calculated in the cart price", priority = 28)
     public void verifyTheFreeGiftIsNotCalculatedInTheCartPrice() {
