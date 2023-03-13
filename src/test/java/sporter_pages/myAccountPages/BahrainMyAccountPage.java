767119ed3de62dc278cbe1ade3df1e4f98801c76
jumahwalaa@gmail.com <W@laa1041994>
2023-03-13 15:53:57 +0300

    Add Bahrain My account test cases

---
+++ src/test/java/sporter_pages/myAccountPages/BahrainMyAccountPage.java
@@ -0,0 +1,24 @@
+/**
+ * @author w.jumaa
+ * @projectName sporter-web-automation
+ * @classDescription
+ */
+
+package sporter_pages.myAccountPages;
+
+import lombok.Getter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+@Getter
+public class BahrainMyAccountPage extends MyAccountPage{
+    public BahrainMyAccountPage(WebDriver webDriver) {
+        super(webDriver);
+        PageFactory.initElements(webDriver, this);
+    }
+
+
+    @FindBy(xpath ="(//li[@class='countryItem_container__StFb3']/a)[4]" )
+    private WebElement bahrainCountry;
+}
