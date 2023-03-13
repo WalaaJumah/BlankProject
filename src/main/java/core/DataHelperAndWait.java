6dc715ef37c85689948306973f15759cb239cf18
jumahwalaa@gmail.com <W@laa1041994>
2023-03-13 14:07:42 +0300

    Add a comment

--- src/main/java/core/DataHelperAndWait.java
+++ src/main/java/core/DataHelperAndWait.java
@@ -18,7 +18,7 @@ import static org.testng.Assert.assertTrue;
 public  class DataHelperAndWait  {
 
     private static int WaitTime=12;
-    //test2
+    //test21add
 
     public static   void waitForElement(WebElement element, WebDriver webDriver) {
         WebDriverWait wait;
