94a559101c9906afcb13b885cc3e70852c023aa8
Wala'a Mohammad <jumahwalaa@gmail.com>
2023-03-27 13:19:10 +0300

    Update the cart rules

--- src/test/java/sporter_test/sportsSupplementsCategoryTestCases/SportsSupplementsCategoryTestCases.java
+++ src/test/java/sporter_test/sportsSupplementsCategoryTestCases/SportsSupplementsCategoryTestCases.java
@@ -137,6 +137,7 @@ public class SportsSupplementsCategoryTestCases extends BaseTest {
         if(sportsSupplementsCategoryPage.getPaginationBtns().size()>3){
         DataHelperAndWait.clickOnElement(sportsSupplementsCategoryPage.getNextPageBtn(),webDriver);
         DataHelperAndWait.clickOnElement(sportsSupplementsCategoryPage.getPreviousPageBtn(),webDriver);
+        DataHelperAndWait.waitForTime(1500);
          Assert.assertFalse(webDriver.getCurrentUrl().contains("p=2"));}
         else{
             System.out.println("There's only one page in the list");
