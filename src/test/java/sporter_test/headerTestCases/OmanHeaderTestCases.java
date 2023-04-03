abffee560b8a0a5d39759cfa6241b16ae9be95cb
jumahwalaa@gmail.com <W@laa1041994>
2023-04-03 13:37:54 +0300

    Add Header Section for all stores

---
+++ src/test/java/sporter_test/headerTestCases/OmanHeaderTestCases.java
@@ -0,0 +1,28 @@
+/**
+ * @author w.jumaa
+ * @projectName QATesting
+ * @classDescription
+ */
+
+package sporter_test.headerTestCases;
+
+import core.BasePage;
+import org.testng.annotations.BeforeClass;
+import sporter_pages.headerSection.HeaderSection;
+import sporter_pages.homepage_classes.EgyptHomePage;
+import sporter_pages.myAccountPages.OmanMyAccountPage;
+
+public class OmanHeaderTestCases extends HeaderTestCases {
+    @BeforeClass(alwaysRun = true)
+    public void switchToOmanStore() {
+        OmanMyAccountPage omanMyAccountPage = new OmanMyAccountPage(webDriver);
+        EgyptHomePage egyptHomePage = new EgyptHomePage(webDriver);
+        egyptHomePage.switchCountry(omanMyAccountPage.getOmanCountry());
+        if (webDriver.getCurrentUrl().contains(egyptHomePage.omanDomain)) {
+            System.out.println("You are in Oman Store");
+        } else {
+            webDriver.navigate().to(BasePage.BaseURL + egyptHomePage.omanDomain);
+            CloseInitialDialog();
+        }
+    }
+}
