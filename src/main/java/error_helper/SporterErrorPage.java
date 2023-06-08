b0c0eb498b64982228668e66d51ced3239463ef7
Wala'a Mohammad <jumahwalaa@gmail.com>
2023-06-08 21:29:51 +0300

    Update the error msgs

--- src/main/java/error_helper/SporterErrorPage.java
+++ src/main/java/error_helper/SporterErrorPage.java
@@ -12,13 +12,13 @@ import org.openqa.selenium.WebDriver;
 public class SporterErrorPage extends BasePage {
     public static final String pageNotFoundTitle = "Sporter.com - Page Not Found";
     public static final String pageNotFoundMsg = "The page you requested was not found on this store. You can go to homepage or search here";
-    public static final String productsCannotFindMsg = "We can't find products matching the selection.";
-    public static final String generatedContentErrorMsg = "We're sorry, an error has occurred while generating this content.";
+    public static final String productsCannotFindMsg = "We can't find products matching the selection";
+    public static final String generatedContentErrorMsg = "We're sorry, an error has occurred while generating this content";
     public static final String error503 = "Error 503 Backend fetch failed";
     public static final String locationError = "Sorry But There is No Available Shipping Methods For your Location";
     public static final String backendError = "Backend fetch failed";
     public static final String pageUnderMaintenanceMsg = "This page is currently under maintenance";
-    public static final String exceptionPageMsg = "An error has happened during application run. See exception log for details.";
+    public static final String exceptionPageMsg = "An error has happened during application run. See exception log for details";
     public static final String exceptionPageMsg2 = "An error occurred on client";
     public static final String offerNotAvailableMsg = "this offer is not available in your country";
     public static final String facebookError = "App not active";
