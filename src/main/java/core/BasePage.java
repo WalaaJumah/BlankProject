a86b6c66eb47515f2fbd122c05a447216dda30d6
Wala'a Mohammad <jumahwalaa@gmail.com>
2023-06-20 10:27:38 +0300

    Update the maintinance validation

--- src/main/java/core/BasePage.java
+++ src/main/java/core/BasePage.java
@@ -194,6 +194,7 @@ public class BasePage {
         Assert.assertFalse(this.getSourcePage().contains(SporterErrorPage.generatedContentErrorMsg), "We're sorry, an error has occurred while generating this content and the URL is " + webDriver.getCurrentUrl());
         Assert.assertFalse(this.getSourcePage().contains(SporterErrorPage.backendError), "503 error and the URL is " + webDriver.getCurrentUrl());
         Assert.assertFalse(this.getSourcePage().contains(SporterErrorPage.error503), "Backend fetch failed and the URL is " + webDriver.getCurrentUrl());
+        Assert.assertFalse(this.getSourcePage().contains(SporterErrorPage.pageUnderMaintenanceMsg), "Backend fetch failed and the URL is " + webDriver.getCurrentUrl());
         this.getStatusCode(currentURL);
     }
 
