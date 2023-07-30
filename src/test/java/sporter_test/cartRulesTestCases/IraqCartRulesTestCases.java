5c7c685406f72df04a1ae8b763371a5f4dfba517
WalaaJumah <jumahwalaa@gmail.com>
2023-07-30 09:58:53 +0300

    Update the Iraq

---
+++ src/test/java/sporter_test/cartRulesTestCases/IraqCartRulesTestCases.java
@@ -0,0 +1,27 @@
+/**
+ * @author w.jumaa
+ * @projectName QATesting
+ * @classDescription
+ */
+
+package sporter_test.cartRulesTestCases;
+
+import core.BasePage;
+import lombok.Getter;
+import org.testng.annotations.BeforeClass;
+import sporter_pages.homepage_classes.IraqHomePage;
+import sporter_test.AccountRegistrationTestCases.AccountRegistrationTestCases;
+@Getter
+public class IraqCartRulesTestCases extends AccountRegistrationTestCases {
+    @BeforeClass(alwaysRun = true)
+    public void IraqCartRulesTestCases() {
+        IraqHomePage iraqHomePage = new IraqHomePage(webDriver);
+        iraqHomePage.switchCountry(iraqHomePage.getIraqCountry());
+        if (webDriver.getCurrentUrl().contains(iraqHomePage.iraqDomain)) {
+            System.out.println("You are in Iraq Store");
+        } else {
+            webDriver.navigate().to(BasePage.BaseURL + iraqHomePage.iraqDomain);
+            CloseInitialDialog();
+        }
+    }
+}
