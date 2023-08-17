0a4ee4a3da79de03c7118bdebf1cec1e82f14eb2
WalaaJumah <jumahwalaa@gmail.com>
2023-08-17 14:53:10 +0300

    Update Login Terst cases

--- src/test/java/sporter_test/loginTestCases/LoginTestCases.java
+++ src/test/java/sporter_test/loginTestCases/LoginTestCases.java
@@ -11,7 +11,9 @@ import core.DataHelperAndWait;
 import core.WebElementsAssertion;
 import org.testng.annotations.Test;
 import sporter_pages.AccountRegistrationPage.AccountRegistrationPage;
+import sporter_pages.footerSections.FooterSection;
 import sporter_pages.headerSection.HeaderSection;
+import sporter_pages.healthy_food_pages.HealthyFoodPage;
 import sporter_pages.homepage_classes.HomePage;
 import sporter_pages.loginPage.LoginPage;
 import xml_reader.XmlReader;
@@ -30,8 +32,15 @@ public class LoginTestCases extends BaseTest {
         DataHelperAndWait.clickOnElement(loginPage.getSignInOption(), webDriver);
         WebElementsAssertion.validateTheCurrentUrlContainsString(loginPage.loginLink, webDriver);
     }
+    @Test(groups = {"1.4 Low Severity"}, description = "{{CountryName}}: Make sure all secure tabs appears correctly(100% Secure Payments+ 100% Authentic Products+Fast Delivery Service blocks)", priority = 2)
+    public void verifyAll3BlocksExistInThaPageBottomSectionAppearsCorrectlyInHealthyFoodCategoryPage(){
+        FooterSection footerSection = new FooterSection(webDriver);
+        for (int i = 0; i < footerSection.getPageBottomBlocksList().size(); i++) {
+            WebElementsAssertion.validateTheElementIsDisplayed(footerSection.getPageBottomBlocksList().get(i), webDriver);
+        }
+    }
 
-    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify All fields, labels & Components appear correctly in Login page", priority = 2)
+    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify All fields, labels & Components appear correctly in Login page", priority = 3)
     public void verifyAllFieldsAppearCorrectlyInLogin() throws IOException {
         LoginPage loginPage = new LoginPage(webDriver);
         AccountRegistrationPage registerPage = new AccountRegistrationPage(webDriver);
@@ -46,8 +55,14 @@ public class LoginTestCases extends BaseTest {
         WebElementsAssertion.validateTheElementIsDisplayed(loginPage.getFaceBookLoginBtn(), webDriver);
         WebElementsAssertion.validateTheElementIsDisplayed(loginPage.getSignInWithSocialMediaLabel(), webDriver);
     }
-
-    @Test(enabled = false,groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Inability to login using wrong email and the correct Error Msg appears", priority = 3)
+    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Make sure ability to navigate to the home page by clicking on the sporter logo  ", priority = 4)
+    public void verifyAbilityToNavigateToHomePageByClickingOnSporterLogoFromPdp() throws IOException {
+        HeaderSection headerSection = new HeaderSection(webDriver);
+        HomePage homePage = new HomePage(webDriver);
+        DataHelperAndWait.clickOnElement(headerSection.getSporterLogo(), webDriver);
+        WebElementsAssertion.validateTheElementIsDisplayed(homePage.getVitaminsAndHealthCategory(), webDriver);
+    }
+    @Test(enabled = false,groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Inability to login using wrong email and the correct Error Msg appears", priority = 5)
     public void verifyInAbilityToLoginUsingWrongEmail() throws IOException {
         LoginPage loginPage = new LoginPage(webDriver);
         loginPage.navigateToLoginPage();
@@ -63,13 +78,13 @@ public class LoginTestCases extends BaseTest {
         }
     }
 
-    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}:  Verify Have Account tab is Active By Default when access Login Page", priority = 4)
+    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}:  Verify Have Account tab is Active By Default when access Login Page", priority = 6)
     public void verifyHaveAccountTabActiveByDefault() {
         AccountRegistrationPage registerPage = new AccountRegistrationPage(webDriver);
         WebElementsAssertion.assertionAttributeTrueForElement(registerPage.getHaveAnAccountTab(), webDriver, "is-active", "1");
     }
 
-    @Test(groups = {"All Smoke Testing Result", "1.1 Critical Severity"}, description = "{{CountryName}}: Ability to login correctly using valid credential", priority = 5)
+    @Test(groups = {"All Smoke Testing Result", "1.1 Critical Severity"}, description = "{{CountryName}}: Ability to login correctly using valid credential", priority = 7)
     public void verifyAbilityToLoginCorrectlyWithValidCredentials() throws IOException {
         LoginPage loginPage = new LoginPage(webDriver);
         loginPage.navigateToLoginPage();
@@ -78,7 +93,7 @@ public class LoginTestCases extends BaseTest {
 
  }
 
-    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Inability to login using correct Email & Wrong Password and the correct Error Msg appears", priority = 6)
+    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Inability to login using correct Email & Wrong Password and the correct Error Msg appears", priority = 8)
     public void verifyInAbilityToLoginUsingCorrectEmailAndWrongPassword() throws IOException {
         LoginPage loginPage = new LoginPage(webDriver);
         webDriver.manage().deleteCookieNamed("uid");
@@ -94,7 +109,7 @@ public class LoginTestCases extends BaseTest {
         }
     }
 
-    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Inability to login without filling Email & Password fields", priority = 7)
+    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Inability to login without filling Email & Password fields", priority = 9)
     public void verifyInAbilityToLoginWithoutEmailAndPassword() throws IOException {
         LoginPage loginPage = new LoginPage(webDriver);
         webDriver.manage().deleteCookieNamed("uid");
@@ -104,7 +119,7 @@ public class LoginTestCases extends BaseTest {
         WebElementsAssertion.checkRequiredErrorMsgIsDisplayed(loginPage.getPasswordErrorMsg(), webDriver);
     }
 
-    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Inability to login without filling Email field only", priority = 8)
+    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Inability to login without filling Email field only", priority = 10)
     public void verifyInAbilityToLoginWithoutEmail() throws IOException {
         LoginPage loginPage = new LoginPage(webDriver);
         webDriver.manage().deleteCookieNamed("uid");
@@ -114,7 +129,7 @@ public class LoginTestCases extends BaseTest {
         WebElementsAssertion.checkRequiredErrorMsgIsDisplayed(loginPage.getEmailErrorMsg(), webDriver);
     }
 
-    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Inability to login without filling Password field only", priority = 9)
+    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Inability to login without filling Password field only", priority = 11)
     public void verifyInAbilityToLoginWithoutPassword() throws IOException {
         LoginPage loginPage = new LoginPage(webDriver);
         webDriver.manage().deleteCookieNamed("uid");
@@ -124,7 +139,7 @@ public class LoginTestCases extends BaseTest {
         WebElementsAssertion.checkRequiredErrorMsgIsDisplayed(loginPage.getPasswordErrorMsg(), webDriver);
     }
 
-    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Inability to login with wrong email format and the correct error Msg appears", priority = 10)
+    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Inability to login with wrong email format and the correct error Msg appears", priority = 12)
     public void verifyInAbilityToLoginWithIncorrectEmailFormat() throws IOException {
         LoginPage loginPage = new LoginPage(webDriver);
         webDriver.manage().deleteCookieNamed("uid");
@@ -139,7 +154,7 @@ public class LoginTestCases extends BaseTest {
         }
     }
 
-    @Test(groups = { "1.3 Medium Severity"}, description = "{{CountryName}}: Verify show password icon works correctly", priority = 11)
+    @Test(groups = { "1.3 Medium Severity"}, description = "{{CountryName}}: Verify show password icon works correctly", priority = 13)
     public void verifyShowPasswordIconWorksCorrectly() throws IOException {
         LoginPage loginPage = new LoginPage(webDriver);
         webDriver.manage().deleteCookieNamed("uid");
@@ -149,14 +164,14 @@ public class LoginTestCases extends BaseTest {
         WebElementsAssertion.assertionAttributeTrueForElement(loginPage.getPasswordField(), webDriver, "type", "text");
     }
 
-    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify hide password icon works correctly", priority = 12)
+    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify hide password icon works correctly", priority = 14)
     public void verifyHidePasswordIconWorksCorrectly() {
         LoginPage loginPage = new LoginPage(webDriver);
         DataHelperAndWait.clickOnElement(loginPage.getHidePasswordIcon(), webDriver);
         WebElementsAssertion.assertionAttributeTrueForElement(loginPage.getPasswordField(), webDriver, "type", "password");
     }
 
-    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Create Account Link works correctly", priority = 13)
+    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Create Account Link works correctly", priority = 15)
     public void verifyCreateAccountLinkWorksCorrectly() {
         LoginPage loginPage = new LoginPage(webDriver);
         AccountRegistrationPage registerPage = new AccountRegistrationPage(webDriver);
@@ -165,7 +180,7 @@ public class LoginTestCases extends BaseTest {
     }
 
     //TODO: There's a bug here & we will revisit it after fixing: https://sporter1.atlassian.net/browse/NS-56
-    @Test(groups = {"All Smoke Testing Result", "1.2 High Severity"}, description = "{{CountryName}}:  Verify ability to login using FaceBook account from Login Page", priority = 14)
+    @Test(groups = {"All Smoke Testing Result", "1.2 High Severity"}, description = "{{CountryName}}:  Verify ability to login using FaceBook account from Login Page", priority = 16)
     public void verifyAbilityToSignInUsingFaceBook() throws IOException {
         try {
             LoginPage loginPage = new LoginPage(webDriver);
@@ -197,7 +212,7 @@ public class LoginTestCases extends BaseTest {
     }
 
     //TODO: There's a bug here & we will revisit it after fixing: https://sporter1.atlassian.net/browse/NS-56
-    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}:  Verify ability to login using FaceBook account from FaceBook Option appearing in Account Menu", priority = 15)
+    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}:  Verify ability to login using FaceBook account from FaceBook Option appearing in Account Menu", priority = 17)
     public void verifyAbilityToSignInUsingFaceBookOptionAppearsInAccountMenu() throws IOException {
         LoginPage loginPage = new LoginPage(webDriver);
         HeaderSection header = new HeaderSection(webDriver);
@@ -219,7 +234,7 @@ public class LoginTestCases extends BaseTest {
 //        }
     }
 
-    @Test(enabled = false,groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify the system keeps the user logged in after switching the country", priority = 16)
+    @Test(enabled = false,groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify the system keeps the user logged in after switching the country", priority = 18)
     public void verifyTheSystemKeepsTheCustomerLoggedInAfterSwitchingTheCountry() throws IOException {
         HeaderSection header = new HeaderSection(webDriver);
         AccountRegistrationPage registerPage = new AccountRegistrationPage(webDriver);
