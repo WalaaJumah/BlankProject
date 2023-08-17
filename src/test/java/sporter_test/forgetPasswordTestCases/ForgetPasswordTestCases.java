c3036260a92c3f3656b5a663718988247be59d7f
WalaaJumah <jumahwalaa@gmail.com>
2023-08-17 13:47:27 +0300

    Update Forget Password

--- src/test/java/sporter_test/forgetPasswordTestCases/ForgetPasswordTestCases.java
+++ src/test/java/sporter_test/forgetPasswordTestCases/ForgetPasswordTestCases.java
@@ -10,6 +10,7 @@ import core.BaseTest;
 import core.DataHelperAndWait;
 import core.WebElementsAssertion;
 import org.testng.annotations.Test;
+import sporter_pages.headerSection.HeaderSection;
 import sporter_pages.loginPage.LoginPage;
 import xml_reader.XmlReader;
 
@@ -29,40 +30,9 @@ public class ForgetPasswordTestCases extends BaseTest {
         DataHelperAndWait.clickOnElement(loginPage.getForgetPasswordLink(), webDriver);
         WebElementsAssertion.validateTheElementIsDisplayed(loginPage.getEmailField(), webDriver);
     }
-
-    @Test(groups = {"All Smoke Testing Result", "1.3 Medium Severity"}, description = "{{CountryName}}: Verify Back To Login Button that appears in the Forget Password page works correctly", priority = 2)
-    public void verifyBackToLoginBtnAppearsInForgetPasswordPageWorksCorrectly() throws IOException {
-        LoginPage loginPage = new LoginPage(webDriver);
-        loginPage.navigateToForgetPassword();
-        loginPage.clickOnBackToLogin();
-    }
-
-    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Inability to click on Submit Button from the Forget Password page without filling Email Address", priority = 3)
-    public void verifyInabilityToClickOnSubmitBtnInForgetPasswordWithoutFillingEmailAddress() throws IOException {
-        LoginPage loginPage = new LoginPage(webDriver);
-        loginPage.navigateToForgetPassword();
-        DataHelperAndWait.clickOnElement(loginPage.getSubmitBtnInForgetPassword(), webDriver);
-        WebElementsAssertion.checkRequiredErrorMsgIsDisplayed(loginPage.getEmailErrorMsgInForgetPassword(), webDriver);
-    }
-
-    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Inability to click on Submit Button with wrong email format and the correct error Msg appears", priority = 4)
-    public void verifyInAbilityToClickOnSubmitInForgetPasswordWithIncorrectEmailFormat() throws IOException {
-        LoginPage loginPage = new LoginPage(webDriver);
-        loginPage.navigateToForgetPassword();
-        DataHelperAndWait.typeTextInElement(loginPage.getEmailField(), webDriver, XmlReader.getXMLData("incorrectEmailFormate"));
-        DataHelperAndWait.clickOnElement(loginPage.getSubmitBtnInForgetPassword(), webDriver);
-        if (webDriver.getCurrentUrl().contains("sporter.com/ar")) {
-            WebElementsAssertion.assertionWebElementConatinsText(loginPage.getEmailErrorMsgInForgetPassword(), webDriver, XmlReader.getXMLData("emailFormatErrorAr"));
-        }
-        if (webDriver.getCurrentUrl().contains("sporter.com/en")) {
-            WebElementsAssertion.assertionWebElementEqualText(loginPage.getEmailErrorMsgInForgetPassword(), webDriver, XmlReader.getXMLData("emailFormatErrorEn"));
-        }
-    }
-
-    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify All fields, labels & Components appear correctly in the Forget Password Page", priority = 5)
+    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify All fields, labels & Components appear correctly in the Forget Password Page", priority = 2)
     public void verifyAllFieldsAppearCorrectlyInForgetPasswordPage() throws IOException {
         LoginPage loginPage = new LoginPage(webDriver);
-        loginPage.navigateToForgetPassword();
         WebElementsAssertion.validateTheElementIsDisplayed(loginPage.getSubmitBtnInForgetPassword(), webDriver);
         WebElementsAssertion.validateTheElementIsDisplayed(loginPage.getBackToLoginLink(), webDriver);
         WebElementsAssertion.validateTheElementIsDisplayed(loginPage.getEmailField(), webDriver);
@@ -71,14 +41,58 @@ public class ForgetPasswordTestCases extends BaseTest {
             WebElementsAssertion.assertionTextIsEqual(loginPage.getForgetPasswordSubHeader(), webDriver, XmlReader.getXMLData("forgetPasswordSubHeaderAr"));
             WebElementsAssertion.assertionTextIsEqual(loginPage.getForgetPasswordDescription(), webDriver, XmlReader.getXMLData("forgetPasswordDescriptionAr"));
         }
-        if (webDriver.getCurrentUrl().contains("sporter.com/en")) {
+        else {
             WebElementsAssertion.assertionTextIsEqual(loginPage.getForgetPasswordHeader(), webDriver, XmlReader.getXMLData("forgetPasswordHeaderEn"));
             WebElementsAssertion.assertionTextIsEqual(loginPage.getForgetPasswordSubHeader(), webDriver, XmlReader.getXMLData("forgetPasswordSubHeaderEn"));
             WebElementsAssertion.assertionTextIsEqual(loginPage.getForgetPasswordDescription(), webDriver, XmlReader.getXMLData("forgetPasswordDescriptionEn"));
         }
     }
+    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Make sure the Cart icon appears correctly", priority = 3)
+    public void verifyCartIconAppearsCorrectly() throws IOException {
+        HeaderSection headerSection = new HeaderSection(webDriver);
+        WebElementsAssertion.validateTheElementIsDisplayed(headerSection.getCartIcon(), webDriver);
+    }
 
-    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Submit button in the Forget Password Page works correctly", priority = 6)
+    @Test(groups = { "1.3 Medium Severity"}, description = "{{CountryName}}: Make sure the My Account icon appears correctly", priority = 4)
+    public void verifyProfileIconAppearsCorrectly() throws IOException {
+        HeaderSection headerSection = new HeaderSection(webDriver);
+        WebElementsAssertion.validateTheElementIsDisplayed(headerSection.getAccountProfileIcon(), webDriver);
+    }
+    @Test(groups = { "1.3 Medium Severity"}, description = "{{CountryName}}: Make sure the Sporter Logo appears correctly", priority = 5)
+    public void verifySporterLogoAppearsCorrectly() throws IOException {
+        HeaderSection headerSection = new HeaderSection(webDriver);
+        WebElementsAssertion.validateTheElementIsDisplayed(headerSection.getSporterLogo(), webDriver);
+    }
+    @Test(groups = {"All Smoke Testing Result", "1.3 Medium Severity"}, description = "{{CountryName}}: Verify Back To Login Button that appears in the Forget Password page works correctly", priority = 6)
+    public void verifyBackToLoginBtnAppearsInForgetPasswordPageWorksCorrectly() throws IOException {
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.navigateToForgetPassword();
+        loginPage.clickOnBackToLogin();
+    }
+
+    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Inability to click on Submit Button from the Forget Password page without filling Email Address", priority = 7)
+    public void verifyInabilityToClickOnSubmitBtnInForgetPasswordWithoutFillingEmailAddress() throws IOException {
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.navigateToForgetPassword();
+        DataHelperAndWait.clickOnElement(loginPage.getSubmitBtnInForgetPassword(), webDriver);
+        WebElementsAssertion.checkRequiredErrorMsgIsDisplayed(loginPage.getEmailErrorMsgInForgetPassword(), webDriver);
+    }
+
+    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Inability to click on Submit Button with wrong email format and the correct error Msg appears", priority = 8)
+    public void verifyInAbilityToClickOnSubmitInForgetPasswordWithIncorrectEmailFormat() throws IOException {
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.navigateToForgetPassword();
+        DataHelperAndWait.typeTextInElement(loginPage.getEmailField(), webDriver, XmlReader.getXMLData("incorrectEmailFormate"));
+        DataHelperAndWait.clickOnElement(loginPage.getSubmitBtnInForgetPassword(), webDriver);
+        if (webDriver.getCurrentUrl().contains("sporter.com/ar")) {
+            WebElementsAssertion.assertionWebElementConatinsText(loginPage.getEmailErrorMsgInForgetPassword(), webDriver, XmlReader.getXMLData("emailFormatErrorAr"));
+        }
+        else{
+            WebElementsAssertion.assertionWebElementEqualText(loginPage.getEmailErrorMsgInForgetPassword(), webDriver, XmlReader.getXMLData("emailFormatErrorEn"));
+        }
+    }
+
+    @Test(groups = {"1.3 Medium Severity"}, description = "{{CountryName}}: Verify Submit button in the Forget Password Page works correctly", priority = 9)
     public void verifySubmitInForgetPasswordPageWorksCorrectly() throws IOException {
         LoginPage loginPage = new LoginPage(webDriver);
         loginPage.navigateToForgetPassword();
@@ -92,4 +106,5 @@ public class ForgetPasswordTestCases extends BaseTest {
             WebElementsAssertion.assertionTextIsEqual(loginPage.getForgetPasswordToastMsg(), webDriver, XmlReader.getXMLData("forgetPasswordToastMsgEn"));
         }
     }
+
 }
