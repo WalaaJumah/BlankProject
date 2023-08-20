34b1733486587c330c67423b8e6f35958ed4cb16
WalaaJumah <jumahwalaa@gmail.com>
2023-08-20 09:23:21 +0300

    Update Iraq guest Checkout cases

--- src/test/java/sporter_test/guestCheckoutCycleTestCases/IraqGuestCheckoutCycleTestCases.java
+++ src/test/java/sporter_test/guestCheckoutCycleTestCases/IraqGuestCheckoutCycleTestCases.java
@@ -7,9 +7,14 @@
 package sporter_test.guestCheckoutCycleTestCases;
 
 import core.BasePage;
+import core.WebElementsAssertion;
 import lombok.Getter;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import sporter_pages.cartPages.CartPage;
 import sporter_pages.homepage_classes.IraqHomePage;
+
+import java.io.IOException;
 
 @Getter
 public class IraqGuestCheckoutCycleTestCases extends GuestCheckoutCycleTestCases{
@@ -23,5 +28,11 @@ public class IraqGuestCheckoutCycleTestCases extends GuestCheckoutCycleTestCases
         }
         storeCountry="IraqMyAccountTestCases";
         countryCode="964";
+    }
+    @Test(groups = { "1.3 Medium Severity"}, description = "{{CountryName}}: Make sure that all payment methods are appear correctly in the Cart page", priority = 5)
+    public void verifyAllPaymentMethodAppearingTheCartPage() throws IOException {
+        CartPage cartPage = new CartPage(webDriver);
+        WebElementsAssertion.validateTheElementIsDisplayed(cartPage.getWeAcceptLabel(), webDriver);
+        WebElementsAssertion.validateTheElementIsDisplayed(cartPage.getCreditCardOption(), webDriver);
     }
 }
