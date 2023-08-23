8e2ed8d48a0a46e9cd09848f0f3ed7a5ef5dda7f
WalaaJumah <jumahwalaa@gmail.com>
2023-08-23 12:10:44 +0300

    AUT-490 Add waitTillAttributeToBe method

--- src/main/java/core/DataHelperAndWait.java
+++ src/main/java/core/DataHelperAndWait.java
@@ -487,7 +487,7 @@ for(int i=0;i<jsErrors.getAll().size()-1;i++){
             return false;
         }
     }
-    public static void waitTillAttributeToBe(WebElement element,String attribute, String attributeValue,WebDriver webDriver{
+    public static void waitTillAttributeToBe(WebElement element,String attribute, String attributeValue,WebDriver webDriver){
         WebDriverWait wait;
         wait = new WebDriverWait(webDriver, Duration.ofSeconds(WaitTime));
         wait.until(ExpectedConditions.attributeToBe(element, attribute, attributeValue));
