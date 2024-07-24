965217d1550b192acb1c9f617c5aba2e7d92c70c
WalaaJumah <jumahwalaa@gmail.com>
2024-07-24 10:48:54 +0300

    update test cases

--- src/test/java/testclass.java
+++ src/test/java/testclass.java
@@ -2,6 +2,7 @@ import core.BaseTest;
 
 import files_factory.excel.ExcelReader;
 import files_reader.PropertiesHelpers;
+import org.testng.annotations.DataProvider;
 import site_pages.login.ArabicLoginPage;
 import helper_classes.AssertionHelper;
 import helper_classes.ElementHelper;
@@ -21,13 +22,25 @@ public class testclass extends BaseTest {
     public testclass() {
         Language = "Ar";
     }
+    @DataProvider(name = "Authentication")
 
+    public static Object[][] credentials() {
+
+        // The number of times data is repeated, test will be executed the same no. of times
+
+        // Here it will execute two times
+
+        return new Object[][] { { "testuser_12", "Test@123" }, { "testuser_1", "Test@123" }};
+
+    }
     //Date Search
-    @Test(groups = {"Priority 3 Modules", "1.1 Critical Severity"}, description = "{{CountryName}}: For Logged-in user, Verify clicking on the Add Apartment option will redirect user to the Add Apartment form", priority = 3)
-    public void verifyClickingOnAddApartmentWorksCorrectly() {
+    @Test(dataProvider = "Authentication", groups = {"Priority 3 Modules", "1.1 Critical Severity"}, description = "{{CountryName}}: For Logged-in user, Verify clicking on the Add Apartment option will redirect user to the Add Apartment form", priority = 3)
+    public void verifyClickingOnAddApartmentWorksCorrectly(String sUsername, String sPassword) {
 
         System.out.println("XML: "+ XmlReader.getXMLData("phoneNumber"));
         System.out.println("Propertie: "+ PropertiesHelpers.getValue("senderEmail"));
+        System.out.println("DataDriven: "+ sUsername);
+        System.out.println("DataDriven: "+ sPassword);
     }
 
 
